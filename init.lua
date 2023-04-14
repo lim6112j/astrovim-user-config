7264d1753a223084bbece19f69412565b5990a1e
ben lim <lim6112j@gmail.com>
2023-04-14 15:57:20 +0900

    user plugin added

---
+++ init.lua
@@ -0,0 +1,13 @@
+return {
+  plugins = {
+    {
+      "kylechui/nvim-surround",
+      lazy = false,
+      config = function()
+        require("nvim-surround").setup({
+          -- Configuration here, or leave empty to use defaults
+        })
+      end,
+    },
+  },
+}
