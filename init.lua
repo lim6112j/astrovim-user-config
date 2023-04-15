e41c2a171cb825ed50abef1cb10dc4bb15ffefa9
lim6112j <lim6112j@gmail.com>
2023-04-16 03:46:48 +0900

    orgmode setup

--- init.lua
+++ init.lua
@@ -1,17 +1,23 @@
 return {
-  plugins = {
-    {
-      "kylechui/nvim-surround",
-      lazy = false,
-      config = function()
-        require("nvim-surround").setup({
-          -- Configuration here, or leave empty to use defaults
-        })
-      end
-    }, { 'christoomey/vim-tmux-navigator', lazy = false }, {
-    'nvim-orgmode/orgmode',
-    lazy = false,
-    config = function() require("orgmode").setup_ts_grammar() end
-  }
-  }
+    plugins = {
+        {
+            "kylechui/nvim-surround",
+            lazy = false,
+            config = function()
+                require("nvim-surround").setup({
+                    -- Configuration here, or leave empty to use defaults
+                })
+            end
+        }, {'christoomey/vim-tmux-navigator', lazy = false}, {
+            'nvim-orgmode/orgmode',
+            lazy = false,
+            config = function()
+                require("orgmode").setup_ts_grammar()
+                require("orgmode").setup({
+                    org_agenda_files = {'~/org/*'},
+                    org_default_notes_file = '~/org/refile.org'
+                })
+            end
+        }
+    }
 }
