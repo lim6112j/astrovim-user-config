0fe41eb5618685f894bb94384883f0bc8ae9eb92
ben lim <lim6112j@gmail.com>
2023-04-20 13:27:48 +0900

    org refile

--- init.lua
+++ init.lua
@@ -12,12 +12,15 @@ return {
             'nvim-orgmode/orgmode',
             lazy = false,
             config = function()
-                require("orgmode").setup_ts_grammar()
                 require("orgmode").setup({
-                    org_agenda_files = {'~/org/agenda.org'},
-                    org_default_notes_file = '~/org/notes.org',
+                    org_agenda_files = {'~/org/*', '~/org/**/*'},
+                    org_default_notes_file = '~/org/refile.org',
                     org_todo_keywords = {
                         'TODO', 'WAITING', '|', 'DONE', 'DELEGATED'
+                    },
+                    
+                    mappings = {
+                    disable_all = true
                     },
                     org_todo_keyword_faces = {
                         WAITING = ':foreground blue :weight bold',
@@ -25,6 +28,8 @@ return {
                         TODO = ':background #000000 :foreground red' -- overrides builtin color for `TODO` keyword
                     }
                 })
+                
+                require("orgmode").setup_ts_grammar()
             end
         }
     }
