bf305efad146b31dec15d5a7f3cf1cb677a0ed9f
lim6112j <lim6112j@gmail.com>
2023-05-02 02:10:22 +0900

    cursorline

--- init.lua
+++ init.lua
@@ -7,6 +7,49 @@ vim.g.neovide_transparency = 0.0
 vim.g.transparency = 1.0
 vim.g.neovide_background_color = "#0f1117" .. alpha()
 vim.o.guifont = "Hack Nerd Font Mono:h20"
+-- cursor line
+
+function Pain()
+    local colors = {
+        bg = "#202328",
+        fg = "#bbc2cf",
+        yellow = "#ECBE7B",
+        cyan = "#008080",
+        darkblue = "#081633",
+        green = "#3d5122",
+        darkgreen = "#156339",
+        orange = "#FF8800",
+        violet = "#a9a1e1",
+        magenta = "#c678dd",
+        blue = "#51afef",
+        red = "#6f3328"
+    }
+
+    local mode_color = {
+        n = colors.darkgreen,
+        i = colors.green,
+        v = colors.yellow,
+        V = colors.blue,
+        c = colors.magenta,
+        no = colors.red,
+        s = colors.orange,
+        S = colors.orange,
+        ic = colors.yellow,
+        R = colors.violet,
+        Rv = colors.violet,
+        cv = colors.red,
+        ce = colors.red,
+        r = colors.cyan,
+        rm = colors.cyan,
+        tr = colors.red
+    }
+    local color = mode_color[vim.api.nvim_get_mode().mode]
+    if color == nil then color = "NONE" end
+    print(vim.api.nvim_get_mode().mode)
+    vim.api.nvim_command("hi! CursorLine guifg=NONE guibg=" .. color)
+end
+
+vim.api.nvim_command([[autocmd ModeChanged * lua Pain()]])
 
 return {
     plugins = {
@@ -38,7 +81,7 @@ return {
                 org_todo_keyword_faces = {
                     WAITING = ':foreground blue :weight bold',
                     DELEGATED = ':background #FFFFFF :slant italic :underline on',
-                    TODO = ':background #000000 :foreground red' -- overrides builtin color for `TODO` keyword
+                    TODO = ':background #000000 :foreground red'     -- overrides builtin color for `TODO` keyword
                 }
             })
         end
