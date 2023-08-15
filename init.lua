b3737d4fb171709849fd9a89a71dace6a1de2e14
ben lim <lim6112j@gmail.com>
2023-08-15 18:31:56 +0900

    merge redo for mistake

--- init.lua
+++ init.lua
@@ -10,6 +10,49 @@ vim.o.guifont = "Hack Nerd Font Mono:h20"
 -- fsharp 쓸때 ionide가 빈줄에서 32603 에러를 쏟아내는거 방지
 vim.cmd([[let g:fsharp#show_signature_on_cursor_move = 0]])
 -- vim.g.fsharp#show_signature_on_cursor_move = 0
+-- cursor line
+function Pain()
+    local colors = {
+        bg = "#202328",
+        fg = "#bbc2cf",
+        yellow = "#ECBE7B",
+        cyan = "#008080",
+        darkblue = "#081633",
+        green = "#3d5122",
+        darkgreen = "#156339",
+        orange = "#FF8800",
+        violet = "#a9a1e1",
+        magenta = "#c678dd",
+        blue = "#51afef",
+        red = "#6f3328"
+    }
+
+    local mode_color = {
+        n = colors.darkgreen,
+        i = colors.green,
+        v = colors.yellow,
+        V = colors.blue,
+        c = colors.magenta,
+        no = colors.red,
+        s = colors.orange,
+        S = colors.orange,
+        ic = colors.yellow,
+        R = colors.violet,
+        Rv = colors.violet,
+        cv = colors.red,
+        ce = colors.red,
+        r = colors.cyan,
+        rm = colors.cyan,
+        tr = colors.red
+    }
+    local color = mode_color[vim.api.nvim_get_mode().mode]
+    if color == nil then color = "NONE" end
+    print(vim.api.nvim_get_mode().mode)
+    vim.api.nvim_command("hi! CursorLine guifg=NONE guibg=" .. color)
+end
+
+vim.api.nvim_command([[autocmd ModeChanged * lua Pain()]])
+
 return {
     plugins = {
         { "vim-scripts/paredit.vim",        lazy = false }, {
@@ -40,7 +83,7 @@ return {
                 org_todo_keyword_faces = {
                     WAITING = ':foreground blue :weight bold',
                     DELEGATED = ':background #FFFFFF :slant italic :underline on',
-                    TODO = ':background #000000 :foreground red'     -- overrides builtin color for `TODO` keyword
+                    TODO = ':background #000000 :foreground red' -- overrides builtin color for `TODO` keyword
                 }
             })
         end
