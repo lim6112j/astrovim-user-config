e8a329f3c04ace55c8e5bd32cc004a8ed6ea2049
ben lim <lim6112j@gmail.com>
2023-10-11 11:11:49 +0900

    telescope merge with astrovim setting and user setting

--- init.lua
+++ init.lua
@@ -97,6 +97,8 @@ return {
             "nvim-telescope/telescope.nvim",
             lazy = false,
             config = function()
+                local actions = require "telescope.actions"
+                local get_icon = require("astronvim.utils").get_icon
                 require('telescope').setup {
                     -- defaults = {
                     --     file_ignore_patterns = {
@@ -104,6 +106,28 @@ return {
                     --     }
                     -- }
                     defaults = {
+
+                        git_worktrees = vim.g.git_worktrees,
+                        prompt_prefix = get_icon("Selected", 1),
+                        selection_caret = get_icon("Selected", 1),
+                        path_display = { "truncate" },
+                        sorting_strategy = "ascending",
+                        layout_config = {
+                            horizontal = { prompt_position = "top", preview_width = 0.55 },
+                            vertical = { mirror = false },
+                            width = 0.87,
+                            height = 0.80,
+                            preview_cutoff = 120,
+                        },
+                        mappings = {
+                            i = {
+                                ["<C-n>"] = actions.cycle_history_next,
+                                ["<C-p>"] = actions.cycle_history_prev,
+                                ["<C-j>"] = actions.move_selection_next,
+                                ["<C-k>"] = actions.move_selection_previous,
+                            },
+                            n = { q = actions.close },
+                        },
                         vimgrep_arguments = {
                             'rg',
                             '--color=never',
