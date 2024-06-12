9078aeaad60eff1857f4ac1dc33c9c21cf9bf141
ben lim <lim6112j@gmail.com>
2024-06-12 22:55:32 +0900

    useless code

--- init.lua
+++ init.lua
@@ -115,7 +115,6 @@ return {
         'nvim-orgmode/orgmode',
         lazy = false,
         config = function()
-            require("orgmode").setup_ts_grammar()
             require("orgmode").setup({
                 org_agenda_files = { '~/org/*', '~/org/**/*' },
                 org_default_notes_file = '~/org/refile.org',
