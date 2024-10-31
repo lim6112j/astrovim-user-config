d632fda454dc74a7d6a4dd32d76ec69952132f9d
byeongcheollim <lim6112j@gmail.com>
2024-10-31 21:46:55 +0900

    org removed

--- init.lua
+++ init.lua
@@ -111,30 +111,32 @@ return {
             })
         end
     }, { 'christoomey/vim-tmux-navigator', lazy = false },
-        { 'ionide/Ionide-vim',              lazy = false }, {
-        'nvim-orgmode/orgmode',
-        lazy = false,
-        config = function()
-            require("orgmode").setup_ts_grammar()
-            require("orgmode").setup({
-                org_agenda_files = { '~/org/*', '~/org/**/*' },
-                org_default_notes_file = '~/org/refile.org',
-                org_foldenabled = false,
-                org_folding = { '<TAB>' },
-                org_todo_keywords = {
-                    'TODO', 'WAITING', '|', 'DONE', 'DELEGATED'
-                },
-                -- mappings = {
-                -- disable_all = true
-                -- },
-                org_todo_keyword_faces = {
-                    WAITING = ':foreground blue :weight bold',
-                    DELEGATED = ':background #FFFFFF :slant italic :underline on',
-                    TODO = ':background #000000 :foreground red' -- overrides builtin color for `TODO` keyword
-                }
-            })
-        end
-    }, {
+        { 'ionide/Ionide-vim',              lazy = false },
+--				{
+--        'nvim-orgmode/orgmode',
+--        lazy = false,
+--        config = function()
+--            require("orgmode").setup_ts_grammar()
+--            require("orgmode").setup({
+--                org_agenda_files = { '~/org/*', '~/org/**/*' },
+--                org_default_notes_file = '~/org/refile.org',
+--                org_foldenabled = false,
+--                org_folding = { '<TAB>' },
+--                org_todo_keywords = {
+--                    'TODO', 'WAITING', '|', 'DONE', 'DELEGATED'
+--                },
+--                -- mappings = {
+--                -- disable_all = true
+--                -- },
+--                org_todo_keyword_faces = {
+--                    WAITING = ':foreground blue :weight bold',
+--                    DELEGATED = ':background #FFFFFF :slant italic :underline on',
+--                    TODO = ':background #000000 :foreground red' -- overrides builtin color for `TODO` keyword
+--                }
+--            })
+--        end
+--    },
+		{
         "nvim-telescope/telescope.nvim",
         lazy = false,
         config = function()
